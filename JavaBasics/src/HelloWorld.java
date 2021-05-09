1f6689b4879752e4edbb0eef4ae3392c634e2c49
Paulo Carneiro <paulotrc@gmail.com>
2021-05-09 03:29:24 -0300

    First commit to revise.

---
+++ JavaBasics/src/HelloWorld.java
@@ -0,0 +1,46 @@
+
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+ * Class to demonstrate the first step to learn Java
+ */
+public class HelloWorld {
+
+    /**
+     * Create a main method that's static and no return to execute a code using or not the parameters.
+     * @param args The params that's possible to use on a Java Execution Program by command-line
+     *             For example: java -jar "some arguments"
+     */
+    public static void main(String[] args) {
+
+        //Just use of print without break line;
+        System.out.print("Hello World");
+
+        //Just breaking a line on console
+        System.out.println();
+
+        //Using String format, some ordinary specifiers are String (%s), Decimal (%d), Floating (%f) and Boolean (%b)
+        System.out.printf("There is a Hello in this %s", "World");
+        System.out.println();
+
+        System.out.printf("There is a %d chars in Hello World", 11);
+        System.out.println();
+
+        System.out.printf("There is no poiting float number like %f to define how many chars are in Hello World", 2.5f);
+        System.out.println();
+
+        System.out.printf("There is a doubt about this is %b or %b in a simple Hello World", true, false);
+        System.out.println();
+
+        //Just use of print with break line;
+        System.out.println("Hello World!");
+
+
+    }
+
+}
